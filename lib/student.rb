27e74ec3385993ce4d2cdd1d04511efe03c8b0e7
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 19:31:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
       WHERE grade = 10
       LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
