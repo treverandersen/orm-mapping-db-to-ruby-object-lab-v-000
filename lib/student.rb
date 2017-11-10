5b6e7b63d31077d0af698a05abe317bb9913806c
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 19:25:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,14 +62,14 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(?)
+  def self.first_X_students_in_grade_10(x)
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT ?
+      LIMIT ? = x
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
