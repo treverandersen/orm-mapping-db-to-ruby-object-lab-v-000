9447469d5d0b8ec29436dcaaef6b4ef9ee959956
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 19:05:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,19 @@ class Student
     end.first
   end
 
+  def self.count_all_students_in_grade_9
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql, name, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
